7e725022b74bc47918050ac9be0a545f59d4bad3
Anton <anton@adc.sh>
2019-02-26 11:26:31 +0300

    add category

--- frontend-admin/pages/AddCategory.jsx
+++ frontend-admin/pages/AddCategory.jsx
@@ -1,5 +1,6 @@
 import { Component } from 'preact'
 import fetch from 'unfetch'
+import Form, { FormGroup, Input, TextArea } from '@depack/form'
 import { Col, FormRow } from '../../frontend/components/Bootstrap'
 import ArticleEditor from '../ArticleEditor'
 
@@ -55,6 +56,9 @@ export default class AddCategory extends Component {
   getHint() {
     return `Фраза для поисковой оптимизации, участвующая в адресах страниц, например, knedv.ru/<strong>${this.state.hint}</strong>/3х-комнатные-квартиры.`
   }
+  onFormChange(values) {
+    const newSeo = values.seo
+  }
   render() {
     const hint = this.getHint()
     const { editing, resetImage } = this.state
@@ -62,32 +66,69 @@ export default class AddCategory extends Component {
       <h1>{this.state.editing ? 'Редактировать' : 'Добавить'} Категорию</h1>
       {editing && this.state.loading && <span className="echo-loader">Loading…</span>}
       {!(editing && this.state.loading) &&
-      <form
-        ref={r => this.form = r}
-        onSubmit={this.submit.bind(this)}>
-        <FormRow name="title" placeholder="Москва Новостройки" label="Название" help="Название для меню слева." required="1" value={this.state.data.title} />
-        <FormRow name="seo" placeholder="москва-новостройки" label="СЕО Название" help={hint} required="1" value={this.state.data.seo} />
-        <FormRow name="description" placeholder="Новая недвижиость в столице России -- это привлекательное предложения для тех, кто собирается строить свое будущее в центре событий." label="Описание" help="Краткое описание для главной страницы." textarea={3} required="1" value={this.state.data.description}/>
-        {editing && !resetImage && <div className="form-group">
-          <label>Изображение</label><br/>
-          <img className="img-fluid" src={this.state.data.cdnImage} />
-          <a href="#" className="btn btn-outline-warning" onClick={(e) => {
-            e.preventDefault()
-            this.setState({ resetImage: true })
-            return false
-          }}>Изменить</a>
-        </div>
-        }
-        {(!editing || resetImage) && <FormRow name="image" label="Изображение" help="Картинка, отображаемая на главной странице." file="1" type="file" required="1"/>}
-        <ArticleEditor article={this.state.article} onSave={(html) => {
-          this.setState({ article: html })
-        }}/>
-        {editing && <input type="hidden" name="id" value={this.props.id}/>}
-        <button type="submit" className="btn btn-primary" disabled={this.state.formLoading}>{ this.state.formLoading ? 'Загрузка...' : `${editing ? 'Сохранить' : 'Добавить'}`}</button>
-        {this.state.error && <div className="alert alert-danger mt-3" role="alert">{this.state.error}</div>}
-        {this.state.success && <div className="alert alert-success mt-3" role="alert">Категория успешно {editing ? 'сохранена' : 'создана'}!</div>}
-      </form>
+        <Form onChange={(values) => {
+          console.log(values)
+          console.log(values.seo)
+        }} ref={r => this.form = r}
+        onSubmit={this.submit.bind(this)}
+        >
+          <FormGroup label="Название" help="Название для меню слева.">
+            <Input name="title" placeholder="Москва Новостройки"
+              value={this.state.data.title} required="1" />
+          </FormGroup>
+          <FormGroup label="СЕО Название" help={hint}>
+            <Input required="1" name="seo" placeholder="москва-новостройки" value={this.state.data.seo}/>
+          </FormGroup>
+          <FormGroup label="Описание" help="Краткое описание для главной страницы.">
+            <TextArea rows="3" required name="description" placeholder="Новая недвижиость в столице России -- это привлекательное предложения для тех, кто собирается строить свое будущее в центре событий.">
+              {this.state.data.description}
+            </TextArea>
+          </FormGroup>
+          {editing && !resetImage && <FormGroup label="Изображение">
+            <br/>
+            <img className="img-fluid" src={this.state.data.cdnImage} />
+            <a href="#" className="btn btn-outline-warning" onClick={(e) => {
+              e.preventDefault()
+              this.setState({ resetImage: true })
+              return false
+            }}>Изменить</a>
+          </FormGroup>
+          }
+          {(!editing || resetImage) && <FormRow name="image" label="Изображение" help="Картинка, отображаемая на главной странице." file="1" type="file" required="1"/>}
+          <ArticleEditor article={this.state.article} onSave={(html) => {
+            this.setState({ article: html })
+          }}/>
+          {editing && <input type="hidden" name="id" value={this.props.id}/>}
+
+          <button type="submit" className="btn btn-primary" disabled={this.state.formLoading}>{this.state.formLoading ? 'Загрузка...' : `${editing ? 'Сохранить' : 'Добавить'}`}</button>
+          {this.state.error && <div className="alert alert-danger mt-3" role="alert">{this.state.error}</div>}
+          {this.state.success && <div className="alert alert-success mt-3" role="alert">Категория успешно {editing ? 'сохранена' : 'создана'}!</div>}
+        </Form>
       }
     </Col>
   }
 }
+
+{/* <form
+  ref={r => this.form = r}
+  onSubmit={this.submit.bind(this)}>
+  <FormRow name="description" placeholder="Новая недвижиость в столице России -- это привлекательное предложения для тех, кто собирается строить свое будущее в центре событий." label="Описание" help="Краткое описание для главной страницы." textarea={3} required="1" value={this.state.data.description}/>
+  {editing && !resetImage && <div className="form-group">
+    <label>Изображение</label><br/>
+    <img className="img-fluid" src={this.state.data.cdnImage} />
+    <a href="#" className="btn btn-outline-warning" onClick={(e) => {
+      e.preventDefault()
+      this.setState({ resetImage: true })
+      return false
+    }}>Изменить</a>
+  </div>
+  }
+  {(!editing || resetImage) && <FormRow name="image" label="Изображение" help="Картинка, отображаемая на главной странице." file="1" type="file" required="1"/>}
+  <ArticleEditor article={this.state.article} onSave={(html) => {
+    this.setState({ article: html })
+  }}/>
+  {editing && <input type="hidden" name="id" value={this.props.id}/>}
+  <button type="submit" className="btn btn-primary" disabled={this.state.formLoading}>{ this.state.formLoading ? 'Загрузка...' : `${editing ? 'Сохранить' : 'Добавить'}`}</button>
+  {this.state.error && <div className="alert alert-danger mt-3" role="alert">{this.state.error}</div>}
+  {this.state.success && <div className="alert alert-success mt-3" role="alert">Категория успешно {editing ? 'сохранена' : 'создана'}!</div>}
+</form> */}
